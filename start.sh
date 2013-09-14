5a20a0a41acc80d641d0c88c3d9b474c1842edbc
root <root@mongodb.(none)>
2013-09-14 13:56:21 +0000

    update

--- start.sh
+++ start.sh
@@ -14,13 +14,16 @@ while true; do
         [Nn]* )     
             echo " "
             
+            echo "Meteoroid Docs: http://${IP_LOCAL}:8181"
+                        
+            echo " "            
+            
             echo "Cloud9 IDE: http://${IP_LOCAL}:3131"
             
             echo "Secure Shell: https://${IP_LOCAL}:12320"
             
             echo "Webmin: https://${IP_LOCAL}:12321"
             
-            echo "Meteoroid Docs: http://${IP_LOCAL}:8181"
             
             echo "SSH/SFTP root@${IP_LOCAL} port 22"
             
@@ -30,7 +33,7 @@ while true; do
             
             /root/local/cloud9/bin/cloud9.sh -p 3131 -l 0.0.0.0 -w / > /dev/null &
             
-            export MONGO_URL=mongodb://localhost:27017/meteorite
+            export MONGO_URL=mongodb://localhost:27017/meteoroid
             
             cd /root/local/meteoroid && mrt --port 8181 > /dev/null &
             exit;;
